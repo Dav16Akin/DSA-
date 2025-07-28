f56b42daf047f30aa8b5312d6e68ce9a9f511019
David Akin <210805089@live.unilag.edu.ng>
2025-07-28 02:20:54 +0100

    writingDSA codes the right way

--- leetcode.js
+++ leetcode.js
@@ -5,5 +5,11 @@ function hasDuplicate(nums) {
 // console.log(hasDuplicate([1, 2, 3, 3]));
 
 function isAnagram(s, t) {
-  let;
+  if (s.length !== t.length) return false;
+  const arr1 = s.split("").sort().join();
+  const arr2 = t.split("").sort().join();
+
+  return arr1 == arr2;
 }
+
+console.log(isAnagram("jar", "jam"));
